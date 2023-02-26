0581a15b46cee165362d143f939e9b380584d249
Reed Fu <rfugithub@outlook.com>
2023-02-26 09:40:33 -0500

    basic routing: home, pagenotfound, and curve

---
+++ src/app/tools/curve/curve.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curve',
+  templateUrl: './curve.component.html',
+  styleUrls: ['./curve.component.css']
+})
+export class CurveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

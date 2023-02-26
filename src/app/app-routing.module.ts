1217b74df6c92fd03d0d351eb076ebf118522748
Reed Fu <rfugithub@outlook.com>
2023-02-26 09:40:33 -0500

    basic routing: home, pagenotfound, and curve

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CurveComponent} from "./tools/curve/curve.component";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "home", component: HomeComponent, data: {title: getToolSiteName("Home")}},
+  {path: "about", component: AboutComponent, data: {title: getToolSiteName("About")}},
+  {path: "curve", component: CurveComponent, data: {title: getToolSiteName("Curve")}},
+  {path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+]
+
+function getToolSiteName(tool: string): string{
+  return "Skynet Plotting Neo " + tool;
+}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolsNavbarComponent } from './tools-navbar/tools-navbar.component';
+import { CurveComponent } from './tools/curve/curve.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolsNavbarComponent,
     MenubarComponent,
+    CurveComponent,
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,

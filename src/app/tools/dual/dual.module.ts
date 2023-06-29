55a8ad15309ecf20749dbec5bd47ca3316a58ea0
Reed Fu <rfugithub@outlook.com>
2023-06-29 17:53:36 +0800

    dual component setup

--- src/app/tools/dual/dual.module.ts
+++ src/app/tools/dual/dual.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DualComponent } from './dual/dual.component';
+import {SimpleDataButtonModule} from "../shared/simple-data-button/simple-data-button.component";
+import {SimpleGraphButtonModule} from "../shared/simple-graph-button/simple-graph-button.component";
 
 
 
@@ -9,7 +11,9 @@ import { DualComponent } from './dual/dual.component';
     DualComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SimpleDataButtonModule,
+    SimpleGraphButtonModule
   ]
 })
 export class DualModule { }

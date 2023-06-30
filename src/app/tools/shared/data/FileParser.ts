dde89b5ccb8d0fbd45ef2652ec8e2700b82e4b6b
Reed Fu <rfugithub@outlook.com>
2023-07-01 00:36:14 +0800

    file upload parsing v2

--- src/app/tools/shared/data/FileParser.ts
+++ src/app/tools/shared/data/FileParser.ts
@@ -1,130 +1,286 @@
 import {Subject} from "rxjs";
 
-export interface MyFileParserStrategy {
-  validateFormat(file: File): boolean;
 
-  readFile(fileText: string, dataKeyMaps: { [key: string]: string }[]): {
-    headers: { [key: string]: string } | undefined,
-    data: any[] | undefined
-  };
+export enum MyFileParserErrors {
+  FORMAT = "format",
+  FIELD = "field",
+  HEADER = "header",
+  DATA = "data",
+  STRATEGY = "strategy",
 }
 
-export class MyFileParser {
-  private readonly strategy: MyFileParserStrategy;
-  private readonly dataKeyMaps: { [key: string]: string }[];
-  private headers: { [key: string]: string } | undefined;
-  private data: any[] | undefined;
-  private statusSubject: Subject<void> = new Subject<void>();
-  /**
-   * Observable to notify when the data is updated
-   */
-  public update$ = this.statusSubject.asObservable();
+export interface HeaderRequirement {
+  key: string;
+  value?: string;
+}
 
-  /**
-   * Construct a MyFileParser
-   * @param fileType - The type of file to parse
-   */
-  constructor(fileType: FileType, dataKeyMaps: { [key: string]: string }[]) {
-    if (fileType === FileType.TXT) {
-      this.strategy = new MyFileParserTXT();
-    } else {
-      this.strategy = new MyFileParserDefault();
-    }
-    this.dataKeyMaps = dataKeyMaps;
-  };
-
-
+export interface MyFileParserStrategy {
   /**
    * Validate the format of the file
    * @param file
    * @return true if the format is valid, false otherwise
    */
-  public isFormatValid(file: File): boolean {
+  validateFormat(file: File): boolean;
+
+  /**
+   * Get the headers of the file as a dictionary
+   * @param fileText
+   * @param headerRequirements
+   * @return the headers of the file as a dictionary or undefined if the headers are not found
+   */
+  getHeaders(fileText: string,
+             headerRequirements: HeaderRequirement[])
+    : { [key: string]: string } | undefined;
+
+  /**
+   * Get the indices of the fields in the file
+   * Emits an error if one or more fields are not found
+   * @param fileText
+   * @param dataKeys
+   * @return the indices of the fields in the file or undefined if one or more fields are not found
+   */
+  getFieldsIndices(fileText: string,
+                   dataKeys: string[])
+    : { [key: string]: number } | undefined;
+
+  /**
+   * Get the data from the file
+   * Emits an error if the data is invalid
+   * Emits the data if the data is valid
+   * @param fileText
+   * @param fields
+   * @param fieldsIndices
+   * @return the data from the file or undefined if the data is invalid
+   */
+  getData(fileText: string,
+          fields: string[],
+          fieldsIndices: { [key: string]: number },)
+    : any[] | undefined;
+
+  /**
+   * Read the file
+   * Emits an error if the file is invalid
+   * Emits the data if the file is valid
+   * @param file
+   * @param headerRequirements
+   * @param dataKeys
+   * @param errorSubject
+   * @param dataSubject
+   * @param headerSubject
+   */
+  readFile(file: File,
+           headerRequirements: HeaderRequirement[],
+           dataKeys: string[],
+           errorSubject: Subject<MyFileParserErrors>,
+           dataSubject: Subject<any> | undefined,
+           headerSubject: Subject<any> | undefined): void;
+}
+
+export class MyFileParser {
+  private readonly strategy!: MyFileParserStrategy;
+  private readonly headerRequirements: HeaderRequirement[];
+  private readonly dataKeys: string[];
+
+  private errorSubject: Subject<MyFileParserErrors> = new Subject<MyFileParserErrors>();
+  public error$ = this.errorSubject.asObservable();
+  private headerSubject: Subject<{ [key: string]: string } | undefined>
+    = new Subject<{ [key: string]: string } | undefined>();
+  public header$ = this.headerSubject.asObservable();
+  private dataSubject: Subject<any> = new Subject<any>();
+  public data$ = this.dataSubject.asObservable();
+
+  constructor(fileType: FileType,
+              dataKeys: string[],
+              headerRequirements: HeaderRequirement[] = [],) {
+    if (fileType === FileType.GBO_SPECTRUM_TXT) {
+      this.strategy = new MyFileParserTXT();
+    } else {
+      this.strategy = new MyFileParserDefault();
+    }
+    this.headerRequirements = headerRequirements;
+    this.dataKeys = dataKeys;
+  }
+
+  /**
+   * Validate the format of a file
+   * @param file
+   * @return true if the format is valid, false otherwise
+   */
+  public validateFormat(file: File): boolean {
     return this.strategy.validateFormat(file);
   }
 
   /**
-   * Load the file into the parser
-   * @param file: File
+   * Get the headers of a file as a dictionary
+   * Emit the headers if the headers are found
+   * @param file
+   * @param isEmitError
    */
-  public loadFile(file: File): void {
-    if (!this.isFormatValid(file))
-      throw new Error("Invalid file format");
-    const fileReader = new FileReader();
-    fileReader.onload = (e) => {
-      const parsed = this.strategy.readFile(fileReader.result as string, this.dataKeyMaps);
-      this.headers = parsed.headers;
-      this.data = parsed.data;
-      this.statusSubject.next();
+  public getHeaders(file: File, isEmitError: false): void {
+    if (this.validateFormat(file)) {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        const fileText = fileReader.result as string;
+        const headers = this.strategy.getHeaders(fileText, this.headerRequirements);
+        if (headers === undefined && isEmitError) {
+          this.errorSubject.next(MyFileParserErrors.HEADER);
+        } else {
+          this.headerSubject.next(headers);
+        }
+      }
+      fileReader.readAsText(file);
     }
-    fileReader.readAsText(file);
   }
 
-  public getData(): any[] | undefined {
-    return this.data;
-  }
-
-  public getHeaders(): { [key: string]: string } | undefined {
-    return this.headers;
+  /**
+   * Read a file, with options weather to emit the data and/or the headers
+   * @param file
+   * @param isEmitData
+   * @param isEmitHeaders
+   */
+  public readFile(file: File, isEmitData: boolean = true, isEmitHeaders: boolean = false): void {
+    this.strategy.readFile(file, this.headerRequirements, this.dataKeys, this.errorSubject,
+      isEmitData ? this.dataSubject : undefined,
+      isEmitHeaders ? this.headerSubject : undefined);
   }
 
 
 }
 
 export enum FileType {
-  TXT = "txt",
+  GBO_SPECTRUM_TXT = "gbo-spectrum-txt",
 }
 
+
 class MyFileParserTXT implements MyFileParserStrategy {
-  validateFormat(file: File): boolean {
-    return (file.type.match("text/plain") as unknown as boolean || file.name.endsWith(".txt"))
-      ? true : false;
+  getData(fileText: string,
+          fields: string[],
+          fieldsIndices: { [p: string]: number }): any[] | undefined {
+    if (fields.length === 0 || (fields.length !== Object.keys(fieldsIndices).length)) {
+      return undefined;
+    }
+
+    const resultData: any[] = [];
+    fileText.split("\n").filter((line: string) => !line.startsWith("#"))
+      .map((line: string) => {
+        const data: any = {};
+        const values: string[] = line.split(/\s+/)
+          .map((value: string) => value.trim())
+          .filter((value: string) => value !== "");
+        fields.forEach((field: string) => {
+          data[field] = values[fieldsIndices[field]];
+        });
+        resultData.push(data);
+      });
+    return resultData;
   }
 
-  readFile(fileText: string, dataKeyMaps: { [key: string]: string }[]): {
-    headers: { [key: string]: string };
-    data: any[]
-  } {
+  getFieldsIndices(fileText: string, dataKeys: string[])
+    : { [p: string]: number } | undefined {
     const lines = fileText.split("\n");
     const nonDataLines = lines.filter((line: string) => line.startsWith("#"));
+    const cols: string[] = nonDataLines[nonDataLines.length - 1]
+      .replace("#", "").split(/\s+/)
+      .map((col: string) => col.trim()).filter((col: string) => col !== "");
+    const keyFieldMap: { [key: string]: number } = {};
+    let isDataKeyMissing: boolean = false;
+    dataKeys.forEach((dataKey: string) => {
+      keyFieldMap[dataKey] = cols.indexOf(dataKey);
+      if (keyFieldMap[dataKey] === -1) {
+        isDataKeyMissing = true;
+      }
+    });
+    return isDataKeyMissing ? undefined : keyFieldMap;
+  }
+
+  getHeaders(fileText: string, headerRequirements: HeaderRequirement[])
+    : { [p: string]: string } | undefined {
+    const lines = fileText.split("\n");
+    const headerLines = lines.filter((line: string) => line.startsWith("#") && line.includes("="));
     const headers: { [key: string]: string } = {};
-    nonDataLines.filter((line: string) => line.includes("=")).map(
-      (line: string): any => {
-        headers[line.split("=")[0].replace("#", "").trim()] = line.split("=")[1].trim();
-      });
-
-    const cols: string[] = nonDataLines[nonDataLines.length - 1].replace("#", "").split(/\s+/).map((col: string) => col.trim()).filter((col: string) => col !== "");
-    const dataKeyIndexMaps: { [key: string]: number } = {};
-    const dataKeys: string[] = [];
-    dataKeyMaps.forEach((dataKeyMap: { [key: string]: string }) => {
-      dataKeys.push(Object.keys(dataKeyMap)[0]);
-      dataKeyIndexMaps[Object.keys(dataKeyMap)[0]] = cols.indexOf(Object.values(dataKeyMap)[0]);
+    headerLines.map((line: string): any => {
+      headers[line.split("=")[0].replace("#", "").trim()]
+        = line.split("=")[1].trim();
     });
 
-    const resultData: any[] =[];
-    lines.filter((line: string) => !line.startsWith("#")).map((line: string) => {
-      const data: any = {};
-      const values: string[] = line.split(/\s+/).map((value: string) => value.trim()).filter((value: string) => value !== "");
-      const entry: any = {};
-      dataKeys.forEach((dataKey: string) => {
-        entry[dataKey] = values[dataKeyIndexMaps[dataKey]];
-      });
-      resultData.push(entry);
+    const headerKeys = Object.keys(headers);
+    let isHeaderValid = true;
+    headerRequirements.forEach((headerRequirement: HeaderRequirement) => {
+      if (!headerKeys.includes(headerRequirement.key) ||
+        (headerRequirement.value !== undefined &&
+          headers[headerRequirement.key] !== headerRequirement.value)) {
+        isHeaderValid = false;
+      }
     });
+    return isHeaderValid ? headers : undefined;
+  }
 
-    return {data: resultData, headers: headers};
+  readFile(file: File, headerRequirements: HeaderRequirement[], dataKeys: string[],
+           errorSubject: Subject<MyFileParserErrors>,
+           dataSubject: Subject<any> | undefined,
+           headerSubject: Subject<any> | undefined): void {
+    if (!this.validateFormat(file)) {
+      errorSubject.next(MyFileParserErrors.FORMAT);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      const fileText = fileReader.result as string;
+      const headers = this.getHeaders(fileText, headerRequirements);
+      if (headers === undefined) {
+        errorSubject.next(MyFileParserErrors.HEADER);
+        return;
+      }
+      if (headerSubject !== undefined) {
+        headerSubject.next(headers);
+      }
+      const fieldsIndices = this.getFieldsIndices(fileText, dataKeys);
+      if (fieldsIndices === undefined) {
+        errorSubject.next(MyFileParserErrors.FIELD);
+        return;
+      }
+
+      const data = this.getData(fileText, dataKeys, fieldsIndices);
+      if (data === undefined) {
+        errorSubject.next(MyFileParserErrors.DATA);
+        return;
+      }
+      if (dataSubject !== undefined) {
+        dataSubject.next(data);
+      }
+    }
+    fileReader.readAsText(file);
+  }
+
+  validateFormat(file: File): boolean {
+    return !(!file.type.match("text/plain") || !file.name.endsWith(".txt"));
   }
 }
 
 class MyFileParserDefault implements MyFileParserStrategy {
+  getData(fileText: string, fields: string[],
+          fieldsIndices: { [value: string]: number }): any[] | undefined {
+    return undefined;
+  }
+
+  getFieldsIndices(fileText: string, dataKeys: string[]): { [p: string]: number } | undefined {
+    return undefined;
+  }
+
+  getHeaders(fileText: string, headerRequirements: HeaderRequirement[]): { [p: string]: string } | undefined {
+    return undefined;
+  }
+
+  readFile(file: File, headerRequirements: HeaderRequirement[], dataKeys: string[],
+           errorSubject: Subject<MyFileParserErrors>,
+           dataSubject: Subject<any> | undefined,
+           headerSubject: Subject<any> | undefined): void {
+    errorSubject.next(MyFileParserErrors.STRATEGY);
+  }
+
   validateFormat(file: File): boolean {
     return false;
   }
 
-  readFile(fileText: string, dataKeyMaps: { [key: string]: string }[]): {
-    headers: { [key: string]: string } | undefined;
-    data: any[] | undefined
-  } {
-    return {data: undefined, headers: undefined}
-  }
 }
--- src/app/tools/spectrum/spectrum/spectrum.component.ts
+++ src/app/tools/spectrum/spectrum/spectrum.component.ts
@@ -13,36 +13,47 @@ import {SpectrumDataDict} from "../spectrum.service.util";
   styleUrls: ['./spectrum.component.scss']
 })
 export class SpectrumComponent implements OnDestroy {
+  private static readonly SPECTRUM_GBO_FIELDS: string[] = ["Freq1(MHz)", "XX1", "YY1"];
   private fileParser: MyFileParser
-    = new MyFileParser(FileType.TXT,
-    [{'wavelength': 'Freq1(MHz)'}, {'channel1': 'XX1'}, {'channel2': 'YY1'}],)
+    = new MyFileParser(FileType.GBO_SPECTRUM_TXT,
+    SpectrumComponent.SPECTRUM_GBO_FIELDS,
+    [{key: "Actual_FREQ1"}],);
   private destroy$: Subject<void> = new Subject<void>();
 
   constructor(
     private service: SpectrumService,
     private honorCodeService: HonorCodePopupService,
     private chartService: HonorCodeChartService,) {
-    this.fileParser.update$.pipe(
+    this.fileParser.error$.pipe(
       takeUntil(this.destroy$)
-    ).subscribe(() => {
-      const dataDictArray: SpectrumDataDict[] = [];
-      this.fileParser.getData()!.map((dataDict: any) => {
-        const freq = parseFloat(dataDict.wavelength);
-        const channel1 = parseFloat(dataDict.channel1);
-        const channel2 = parseFloat(dataDict.channel2);
-        if (!isNaN(freq) && !isNaN(channel1)) {
-          const wl = 299792458 / (freq * 1e4);
-          if (wl >= 21.085 && wl <= 21.125) {
-            dataDictArray.push({
-              wavelength: wl,
-              channel1: isNaN(channel1) ? null : channel1,
-              channel2: isNaN(channel2) ? null : channel2,
-            })
+    ).subscribe(
+      (error: any) => {
+        alert("error " + error);
+      }
+    );
+    this.fileParser.data$.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(
+      (data: any[]) => {
+        const dataDictArray: SpectrumDataDict[] = [];
+        data.map((rawData: any) => {
+          const freq = parseFloat(rawData[SpectrumComponent.SPECTRUM_GBO_FIELDS[0]]);
+          const channel1 = parseFloat(rawData[SpectrumComponent.SPECTRUM_GBO_FIELDS[1]]);
+          const channel2 = parseFloat(rawData[SpectrumComponent.SPECTRUM_GBO_FIELDS[2]]);
+          if (!isNaN(freq) && !isNaN(channel1)) {
+            const wl = 299792458 / (freq * 1e4);
+            if (wl >= 21.085 && wl <= 21.125) {
+              dataDictArray.push({
+                wavelength: wl,
+                channel1: isNaN(channel1) ? null : channel1,
+                channel2: isNaN(channel2) ? null : channel2,
+              })
+            }
           }
-        }
-      });
-      this.service.setData(dataDictArray);
-    });
+        });
+        this.service.setData(dataDictArray);
+      }
+    );
   }
 
   actionHandler(actions: ChartAction[]) {
@@ -62,7 +73,7 @@ export class SpectrumComponent implements OnDestroy {
   }
 
   uploadHandler($event: File) {
-    this.fileParser.loadFile($event);
+    this.fileParser.readFile($event, true);
   }
 
   ngOnDestroy(): void {

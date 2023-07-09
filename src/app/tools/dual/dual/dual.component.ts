1c08bbc11e1e353a3d679c927283d677728e81d5
Reed Fu <rfugithub@outlook.com>
2023-07-09 13:32:39 +0800

    ASTRMNCR-2 refactor dual style and make chart form a popup

--- src/app/tools/dual/dual/dual.component.ts
+++ src/app/tools/dual/dual/dual.component.ts
@@ -3,16 +3,19 @@ import {ChartAction} from "../../shared/types/actions";
 import {HonorCodePopupService} from "../../shared/honor-code-popup/honor-code-popup.service";
 import {HonorCodeChartService} from "../../shared/honor-code-popup/honor-code-chart.service";
 import {DualService} from "../dual.service";
+import {MatDialog} from "@angular/material/dialog";
+import {DualChartFormComponent} from "../dual-chart-form/dual-chart-form.component";
 
 @Component({
   selector: 'app-dual',
   templateUrl: './dual.component.html',
-  styleUrls: ['./dual.component.scss']
+  styleUrls: ['./dual.component.scss', "../../shared/interface/tools.scss"]
 })
 export class DualComponent {
   constructor(private service: DualService,
               private honorCodeService: HonorCodePopupService,
-              private chartService: HonorCodeChartService) {
+              private chartService: HonorCodeChartService,
+              public dialog: MatDialog) {
   }
 
   actionHandler(actions: ChartAction[]) {
@@ -20,14 +23,23 @@ export class DualComponent {
       if (action.action === "addRow") {
         this.service.addRow(-1, 1);
       } else if (action.action === "saveGraph") {
-        this.honorCodeService.honored().subscribe((name: string) => {
-          this.chartService.saveImageHighChartOffline(this.service.getHighChart(), "dual", name);
-        })
+        this.saveGraph();
       } else if (action.action === "resetData") {
         this.service.resetData();
-      } else if (action.action === "resetChartInfo") {
-        this.service.resetChartInfo();
+      } else if (action.action === "editChartInfo") {
+        const dialogRef =
+          this.dialog.open(DualChartFormComponent, {width: 'fit-content'});
+        dialogRef.afterClosed().pipe().subscribe(result => {
+          if (result === "saveGraph")
+            this.saveGraph();
+        });
       }
     });
+  }
+
+  private saveGraph() {
+    this.honorCodeService.honored().subscribe((name: string) => {
+      this.chartService.saveImageHighChartOffline(this.service.getHighChart(), "dual", name);
+    })
   }
 }

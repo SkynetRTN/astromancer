d2776255142c454e26a719ac9318131be94e0ff4
Reed Fu <rfugithub@outlook.com>
2023-07-18 00:26:29 +0800

    ASTRMNCR-28 Drag and Drop: Raw File Parsing

--- src/app/tools/cluster/data-source/cluster-data-source.service.ts
+++ src/app/tools/cluster/data-source/cluster-data-source.service.ts
@@ -1,10 +1,18 @@
 import {Injectable} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {ClusterDataSourceStepper, ClusterDataSourceStepperImpl} from "./cluster-data-source.service.uti";
+import {MyFileParser} from "../../shared/data/FileParser/FileParser";
+import {FileType} from "../../shared/data/FileParser/FileParser.util";
+import {Subject} from "rxjs";
+import {ClusterRawData} from "./cluster-data-source.service.util";
 
 @Injectable()
 export class ClusterDataSourceService implements ClusterDataSourceStepper {
+  public rawData$: Subject<ClusterRawData[]> = new Subject<ClusterRawData[]>();
   private readonly dataSourceStepperImpl: ClusterDataSourceStepper = new ClusterDataSourceStepperImpl();
+  private readonly fileParser: MyFileParser = new MyFileParser(FileType.CSV,
+    ['id', 'filter', 'calibrated_mag', 'mag_error', 'ra_hours', 'dec_degs'])
+  private rawData: ClusterRawData[] = [];
 
   constructor() {
   }
@@ -18,7 +26,20 @@ export class ClusterDataSourceService implements ClusterDataSourceStepper {
   }
 
   onFileUpload(file: File): void {
-    console.log(file);
+    this.fileParser.data$.subscribe(
+      data => {
+        console.log(data);
+        this.setRawData(data);
+      });
+    this.fileParser.error$.subscribe(
+      error => {
+        alert("File Upload Error: " + error);
+      });
+    this.fileParser.readFile(file, true);
   }
 
+  private setRawData(rawData: ClusterRawData[]): void {
+    this.rawData = rawData;
+    this.rawData$.next(this.rawData);
+  }
 }
---
+++ src/app/tools/cluster/data-source/cluster-data-source.service.util.ts
@@ -0,0 +1,9 @@
+
+export interface ClusterRawData {
+  id: string;
+  filter: string;
+  calibrated_mag: string;
+  mag_error: string;
+  ra_hours: string;
+  dec_degs: string;
+}

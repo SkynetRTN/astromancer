663831b81f1ce8925f78a019c8f6149021f69061
Reed Fu <rfugithub@outlook.com>
2023-08-13 14:09:24 -0400

    ASTRMNCR-42 about page: update to ng 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import {NgModule} from '@angular/core';
+import {ANIMATION_MODULE_TYPE, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToolsNavbarModule} from './shared/tools-navbar/tools-navbar.component';
 import {registerAllModules} from 'handsontable/registry';
 import {HomeComponent} from "./shared/home/home.component";
 import {AboutComponent} from "./shared/about/about.component";
 import {PageNotFoundComponent} from "./shared/page-not-found/page-not-found.component";
 import {HonorCodePopupModule} from "./tools/shared/honor-code-popup/honor-code-popup.module";
-import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
-import {MatLegacySelectModule as MatSelectModule} from "@angular/material/legacy-select";
 import {ReactiveFormsModule} from "@angular/forms";
 import {AppearanceStorageService} from "./shared/settings/appearance/service/appearance-storage.service";
-import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
 import {AppearanceService} from "./shared/settings/appearance/service/appearance.service";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 
 registerAllModules();

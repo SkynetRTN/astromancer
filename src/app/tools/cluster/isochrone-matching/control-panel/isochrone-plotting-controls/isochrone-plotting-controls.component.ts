53035a403827c64a0ca8798fb247be27b42fe864
Reed Fu <rfugithub@outlook.com>
2023-12-29 18:22:32 -0500

    ASTRMNCR-84 Isochrone Matching: initialize parameters for isochrone controls

--- src/app/tools/cluster/isochrone-matching/control-panel/isochrone-plotting-controls/isochrone-plotting-controls.component.ts
+++ src/app/tools/cluster/isochrone-matching/control-panel/isochrone-plotting-controls/isochrone-plotting-controls.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ClusterService} from "../../../cluster.service";
 
 @Component({
-  selector: 'app-isochrone-plotting-controls',
-  templateUrl: './isochrone-plotting-controls.component.html',
-  styleUrls: ['./isochrone-plotting-controls.component.scss']
+    selector: 'app-isochrone-plotting-controls',
+    templateUrl: './isochrone-plotting-controls.component.html',
+    styleUrls: ['./isochrone-plotting-controls.component.scss']
 })
 export class IsochronePlottingControlsComponent {
+    defaultDistance: number = 0.1;
+
+    constructor(private service: ClusterService) {
+        this.updateDistance();
+        this.service.fsrParams$.subscribe(() => {
+            this.updateDistance();
+        });
+
+    }
+
+    private updateDistance() {
+        const distance = this.service.getFsrParams().distance
+        if (distance) {
+            this.defaultDistance = (distance.max + distance.min) / 2;
+        } else {
+            this.defaultDistance = 0.1;
+        }
+    }
 
 }

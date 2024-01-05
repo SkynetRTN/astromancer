7b601b08f71419c7422cdcf52095881d7e56e860
Reed Fu <rfugithub@outlook.com>
2024-01-05 17:14:17 -0600

    cluster bug fixes: Result summary layout temp solution

--- src/app/tools/cluster/result/result/result.component.ts
+++ src/app/tools/cluster/result/result/result.component.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'app-result',
-    templateUrl: './result.component.html',
-    styleUrls: ['./result.component.scss', '../../../shared/interface/tools.scss']
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['./result.component.scss']
 })
 export class ResultComponent {
+
+  constructor() {
+  }
 
 }

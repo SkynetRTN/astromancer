e5b1e028dc636c8aef09438737bd23885cb33bc8
Reed Fu <rfugithub@outlook.com>
2024-03-04 17:07:45 -0500

    remove unused imports

--- src/app/tools/cluster/data-source/pop-ups/error/error.component.ts
+++ src/app/tools/cluster/data-source/pop-ups/error/error.component.ts
@@ -1,16 +1,15 @@
 import {Component, Inject} from '@angular/core';
 import {HttpErrorResponse} from "@angular/common/http";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
-import {ok} from "assert";
 
 @Component({
-  selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss', '../cluster-data-source-pop-ups.scss']
+    selector: 'app-error',
+    templateUrl: './error.component.html',
+    styleUrls: ['./error.component.scss', '../cluster-data-source-pop-ups.scss']
 })
 export class ErrorComponent {
-  protected readonly JSON = JSON;
+    protected readonly JSON = JSON;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { error: HttpErrorResponse | null, message: string | null }) {
-  }
+    constructor(@Inject(MAT_DIALOG_DATA) public data: { error: HttpErrorResponse | null, message: string | null }) {
+    }
 }

7a2c763390a1240f7b828c9313cf9c498ee39948
Reed Fu <rfugithub@outlook.com>
2024-03-07 22:00:13 -0500

    change dev astronomicon url to point at localhost for testing

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://astromancer-dev.skynet.unc.edu/api',
-    astronomiconUrl: 'https://astronomicon.skynet.unc.edu',
-    astronomiconApiUrl: 'https://astronomicon.skynet.unc.edu/api'
+    astronomiconUrl: 'http://localhost:4200',
+    astronomiconApiUrl: 'http://127.0.0.1:5000'
 };

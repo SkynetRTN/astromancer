7f6bc1e84d24aaaee04b0334928d69fa6e87fb8b
ReeceClark2 <reececlark55@gmail.com>
2024-10-07 15:36:19 -0400

    create necessary functions for radio search

--- src/app/tools/radiosearch/storage/radiosearch-storage.service.util.ts
+++ src/app/tools/radiosearch/storage/radiosearch-storage.service.util.ts
@@ -5,6 +5,18 @@ export interface Source {
     ra: number;    // Right Ascension (in degrees)
     dec: number;   // Declination (in degrees)
   }
+
+export interface HiddenResults {
+  name: string;
+  MHz38: number | 'Unknown';
+  MHz159: number | 'Unknown';
+  MHz178: number | 'Unknown';
+  MHz750: number | 'Unknown';
+  L1400: number | 'Unknown';
+  S2695: number | 'Unknown';
+  C5000: number | 'Unknown';
+  X8400: number | 'Unknown';
+}
   
 export interface RadioSearchStorageObject {
     dataSource: DataSourceStorageObject

c6d536a72e0a89870879ba57b9a7332b1f14923a
ReeceClark2 <reececlark55@gmail.com>
2025-01-05 11:31:29 -0500

    Add pulsar routing between pages

---
+++ src/app/tools/pulsar/pulsar/pulsar.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnDestroy} from '@angular/core';
+import {PulsarService} from "../pulsar.service";
+import {Subject, takeUntil} from "rxjs";
+
+@Component({
+  selector: 'app-pulsar',
+  templateUrl: './pulsar.component.html',
+  styleUrls: ['./pulsar.component.scss'],
+})
+export class PulsarComponent implements OnDestroy {
+  lightCurveFormValid: boolean = false;
+  pulsarTabindex: number = 0;
+  // this.pulsarTabindex = this.service.getTabIndex();
+  private destroy$: Subject<void> = new Subject<void>();
+
+  constructor(private service: PulsarService) {
+
+    // this.service.interface$.pipe(
+    //   takeUntil(this.destroy$)
+    // ).subscribe(() => {
+    //   this.lightCurveFormValid = this.service.getIsLightCurveOptionValid();
+    // });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+
+  onTabChange($event: number) {
+    this.service.setTabIndex($event);
+  }
+}

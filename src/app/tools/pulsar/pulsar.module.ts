1b322e1ae5807961498a91571ef4267dadb64195
ReeceClark2 <reececlark55@gmail.com>
2025-04-03 22:47:45 -0400

    fix formatting

--- src/app/tools/pulsar/pulsar.module.ts
+++ src/app/tools/pulsar/pulsar.module.ts
@@ -11,9 +11,7 @@ import {SimpleGraphButtonModule} from "../shared/simple-graph-button/simple-grap
 import {PulsarService} from "./pulsar.service";
 import {PulsarTableComponent} from './light-curve/pulsar-table/pulsar-table.component';
 import {HotTableModule} from "@handsontable/angular";
-import {
-  PulsarLightCurveFormComponent
-} from './light-curve/pulsar-light-curve-form/pulsar-light-curve-form.component';
+import {PulsarLightCurveFormComponent} from './light-curve/pulsar-light-curve-form/pulsar-light-curve-form.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {MatOptionModule} from "@angular/material/core";
@@ -21,27 +19,15 @@ import {MatSelectModule} from "@angular/material/select";
 import { PulsarLightCurveHighchartComponent } from './light-curve/pulsar-light-curve-highchart/pulsar-light-curve-highchart.component';
 import {HighchartsChartModule} from "highcharts-angular";
 import {PulsarPeriodogramComponent} from './periodogram/pulsar-periodogram/pulsar-periodogram.component';
-import {
-  PulsarPeriodogramHighchartsComponent
-} from './periodogram/pulsar-periodogram-highcharts/pulsar-periodogram-highcharts.component';
-import {
-  PulsarPeriodogramFormComponent
-} from './periodogram/pulsar-periodogram-form/pulsar-periodogram-form.component';
-import {
-  PulsarPeriodFoldingComponent
-} from './period-folding/pulsar-period-folding/pulsar-period-folding.component';
-import {
-  PulsarPeriodFoldingFormComponent
-} from './period-folding/pulsar-period-folding-form/pulsar-period-folding-form.component';
-import {
-  PulsarPeriodFoldingHighchartComponent
-} from './period-folding/pulsar-period-folding-highchart/pulsar-period-folding-highchart.component';
+import {PulsarPeriodogramHighchartsComponent} from './periodogram/pulsar-periodogram-highcharts/pulsar-periodogram-highcharts.component';
+import {PulsarPeriodogramFormComponent} from './periodogram/pulsar-periodogram-form/pulsar-periodogram-form.component';
+import {PulsarPeriodFoldingComponent} from './period-folding/pulsar-period-folding/pulsar-period-folding.component';
+import {PulsarPeriodFoldingFormComponent} from './period-folding/pulsar-period-folding-form/pulsar-period-folding-form.component';
+import {PulsarPeriodFoldingHighchartComponent} from './period-folding/pulsar-period-folding-highchart/pulsar-period-folding-highchart.component';
 import {InterfaceUtilModule} from "../shared/interface/util";
 import {RouterModule, Routes} from "@angular/router";
 import {MatDialogModule} from "@angular/material/dialog";
-
 import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { PulsarLightCurveChartFormComponent } from './light-curve/pulsar-light-curve-chart-form/pulsar-light-curve-chart-form.component';
 
 

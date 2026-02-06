0d2f52461137e2ae7b6a4add7903e081b910984f
Reed Fu <rfugithub@outlook.com>
2026-02-06 15:50:31 -0500

    refactor home for multiple chart implementations

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { AppearanceService } from "../settings/appearance/service/appearance.service";
+import { ChartType } from "../settings/appearance/service/appearance.utils";
 
 /**
  * Home page of the site
@@ -9,5 +11,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./home.component.scss', '../../tools/shared/interface/tools.scss']
 })
 export class HomeComponent {
+
+  protected readonly ECHART_TOOLS_WHITELIST = ['curve', 'moon', 'venus', 'scatter', 'spectrum', 'galaxy', 'dual'];
+
+  constructor(private appearanceService: AppearanceService) {
+  }
+
+  protected shouldShowEcharts(tool: string): boolean {
+    return this.appearanceService.getChartType() === ChartType.ECHARTS && this.ECHART_TOOLS_WHITELIST.includes(tool);
+  }
 
 }
